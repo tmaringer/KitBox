a42d227d09134ac2699daa7515283753821a1a55
Maringer Thigaut <17068@ecam.be>
2020-03-20 20:18:25 +0100

    package update
    Co-Authored-By: olivierdg2 <olivierdg2@users.noreply.github.com>

--- ShopInterface/Program.cs
+++ ShopInterface/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using projectCS;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,9 +14,44 @@ namespace ShopInterface
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            foreach(string i in DBUtils.RefList("CupboardId", "cupboards where OrderId = \"5\""))
+            {
+                foreach (string j in DBUtils.RefList("BoxeId", "boxes where CupboardId = \"" + i + "\""))
+                {
+                    foreach (string k in DBUtils.RefList("Code", "doors where BoxeId= \"" + j + "\""))
+                    {
+                        //Modify door property
+                        Console.WriteLine("Cupboard " + i + ", Box " + j + ", Door code: " + k);
+                    }
+                    foreach (string k in DBUtils.RefList("Code", "crossbars where BoxeId= \"" + j + "\""))
+                    {
+                        //Modify crossbar property
+                        Console.WriteLine("Cupboard " + i + ", Box " + j + ", Crossbar code: " + k);
+                    }
+                    foreach (string k in DBUtils.RefList("Code", "panels where BoxeId= \"" + j + "\""))
+                    {
+                        //Modify panels property
+                        Console.WriteLine("Cupboard " + i + ", Box " + j + ", Panel code: " + k);
+                    }
+                    foreach (string k in DBUtils.RefList("Code", "cleats where BoxeId= \"" + j + "\""))
+                    {
+                        //Modify cleats property
+                        Console.WriteLine("Cupboard " + i + ", Box " + j + ", Cleat code: " + k);
+                    }
+                    //Modify box property
+                    Console.WriteLine("Box Height:" + DBUtils.RefList("Hauteur", "boxes where CupboardId= \"" + i + "\"")[0]);
+                }
+                foreach (string j in DBUtils.RefList("Code", "angles where CupboardId= \"" + i + "\""))
+                {
+                    //modify Angle bracket property
+                    Console.WriteLine("Cupboard " + i + ", Angle bracket " + j);
+                }
+                //Modify Cupboard property
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ ShopInterface/Sandbox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using projectCS;
+namespace ShopInterface
+{
+    class Sandbox
+    {
+        static void Main()
+        {
+            foreach (string i in DBUtils.RefList("CupboardId", "cupboards where OrderId = \"3\""))
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
