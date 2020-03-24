bd2cc9990ecb1b6bc80cf44e9ef328bff27a90a3
MarcHarony <nathan.vico97@gmail.com>
2020-03-24 18:20:01 +0100

    covfefe2

--- kitbox_user_interface/Form1.cs
+++ kitbox_user_interface/Form1.cs
@@ -384,12 +384,20 @@ namespace kitbox_user_interface_V1
             {
                 int width = Int32.Parse(comboBox4.SelectedItem.ToString());//ça a l'air con mais ça marche
                 int depth = Int32.Parse(comboBox5.SelectedItem.ToString());
+                string color1 = comboBox7.SelectedItem.ToString();//va servir à créer les anglebrackets
+                /*
+                foreach (string colors in projectCS.Color)
+                {
 
+                }
+                */
                 projectCS.Color default_color = projectCS.Color.black;
                 projectCS.Size default_size = new projectCS.Size(0, 0, 0);
                 projectCS.Size traverse_size = new projectCS.Size(0, 0, 0);
                 projectCS.Size panneau_size = new projectCS.Size(0, width, depth);
                 //Size (height, width, depth) 
+
+
                 CrossBar traverseAV = new CrossBar(0, "null", "0000", traverse_size, false, width, default_color);
                 CrossBar traverseAR = new CrossBar(0, "null", "0000", traverse_size, false, width, default_color);
                 Pannel panneauH = new Pannel(0, "null", "0000", panneau_size, false, 0, default_color);
