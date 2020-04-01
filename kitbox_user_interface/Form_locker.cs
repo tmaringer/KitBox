19a4a58e406ad5c362a452c5c07d82e5be4b1705
MarcHarony <nathan.vico97@gmail.com>
2020-04-01 15:11:38 +0200

    test width

--- kitbox_user_interface/Form_locker.cs
+++ kitbox_user_interface/Form_locker.cs
@@ -27,8 +27,9 @@ namespace kitbox_user_interface_V1
         {
             
             ComponentColor c =ShoppingCart.cupboard.colorAngleBracket;
+            int width = ShoppingCart.cupboard.width;
             AngleBracket a = ShoppingCart.cupboard.getAngleBracket();
-            ErrorWindow test = new ErrorWindow(ColorParse.parseToStr(c));
+            ErrorWindow test = new ErrorWindow(ColorParse.parseToStr(c)+width.ToString());
             //ErrorWindow test2 = new ErrorWindow(a.ToString());
             test.displayWindow();
             //test2.displayWindow();
